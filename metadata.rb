1e6436b9d79cdc54160c57f2fbef89ed0ccdf92d
Dmitry Medvedev <seneth@gmail.com>
2015-08-21 14:04:03 +0300

    Authors

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'tinc'
-maintainer 'Maciej Pasternacki'
-maintainer_email 'maciej@3ofcoins.net'
+maintainer 'OTT Development Team'
+maintainer_email 'operations@onetwotrip.com'
 license 'MIT'
 description 'Installs and configures Tinc VPN'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
